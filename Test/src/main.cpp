23a7ca3e740fb72a05c24bb309e5d91446a679ee
bocqueraz <cbocqueraz@gmail.com>
2026-01-24 01:30:02 +0100

    Optimisation de l'usage de l'ESP32
    - Le Serveur Web tourne maintenant sur le core 0 de l'ESP 32 pour éviter la surcharge du core 1.
    - Le core 1 servira donc au controle des moteurs.

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,53 +1,81 @@
+#pragma region Librairies
 #include <WiFi.h>
 #include <WebServer.h>
 #include "index.h"
+#pragma endregion
 
-#ifndef LED
-#define LED 2 // pin de la LED intégrée pour ESP32
-#endif
+#pragma region Logger
+bool disponible = true;
 
+void Logger(String message)
+{
+  if (disponible)
+  {
+    disponible = false;
+    int coreID = xPortGetCoreID(); // récupère l'id du core
+
+    Serial.print("[Core ");
+    Serial.print(coreID);
+    Serial.print("] ");
+    Serial.println(message);
+
+    disponible = true;
+  }
+}
+#pragma endregion
+
+#pragma region Tâche du Serveur Web en mode Point Accès (AP) | sur core 0
 const char *ssid = "Exodus";       // Nom du réseau Wi-Fi
 const char *password = "Exodus25"; // Mot de passe du réseau Wi-Fi
 
 WebServer server(80);
-
-void handleRoot()
+void handleRoot() // page d'accueil
 {
-  server.send(200, "text/html", htmlIndex); // page d'accueil
+  server.send(200, "text/html", htmlIndex);
 }
 
-// void handleOn()
-// {
-//   digitalWrite(LED, HIGH);
-//   server.send(200, "text/html", htmlOn); // page après allumage
-// }
+void tacheServeurWeb(void *parameter) // tâche du serveur web
+{
 
-// void handleOff()
-// {
-//   digitalWrite(LED, LOW);
-//   server.send(200, "text/html", htmlOff); // page après extinction
-// }
+  // Setup() de la tâche
+  WiFi.softAP(ssid, password);
+  Logger("Nom du réseau: " + String(ssid));
+  Logger("Adresse IP de l'AP: " + WiFi.softAPIP().toString());
+
+  server.on("/", handleRoot);
+  server.begin();
+  Logger("Site Internet démarré");
+
+  // Loop() de la tâche
+  for (;;)
+  {
+    server.handleClient();
+
+    delay(2); // permet d'éviter le watchdog timer reset | en gros de faire crash l'ESP32
+  }
+}
+#pragma endregion
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("Démarrage du système...");
+  // Execute la tache sur le core 0
+  xTaskCreatePinnedToCore(
+      tacheServeurWeb, // fonction
+      "ServeurWeb",    // nom
+      10000,           // taille de la pile (valeur arbitraire, je ne sais pas trop ce qu'il faut mettre)
+      NULL,            // paramètres
+      1,               // priorité
+      NULL,            // ??? j'ai pas compris ce que sait
+      0                // core
+  );
 
-  WiFi.softAP(ssid, password);
-  Serial.print("Nom du réseau: ");
-  Serial.println(ssid); // impression du nom du réseau
-  Serial.print("Adresse IP de l'AP: ");
-  Serial.println(WiFi.softAPIP()); // impression de l'adresse IP du site
-
-  // Configuration du routeur web
-  server.on("/", handleRoot); // page d'accueil
-  // server.on("/on", handleOn);   // page après allumage
-  // server.on("/off", handleOff); // page après extinction
-  server.begin();
-  Serial.println("Site Internet démarré");
+  Logger("Tâche Web lancée sur Core 0.");
 }
 
 void loop()
 {
-  server.handleClient();
+  // Il faudra implémenter la logique du controle du moteur ici
+  // Il tournera sur le core 1 de l'ESP32
 }
